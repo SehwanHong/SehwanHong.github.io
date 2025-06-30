6827055980fc95feb2ab11c626026ebb6e1d4cb8
Sehwan Hong <samhong657@gmail.com>
2025-06-30 15:46:36 +0900

    태그 수집을 위한 헬퍼 모듈을 추가하고, TagGenerator에서 태그를 수집하여 페이지 생성을 개선했습니다. 생성된 태그를 사이트 전역에서 사용할 수 있도록 설정했습니다.

--- _plugins/tag_generator.rb
+++ _plugins/tag_generator.rb
@@ -15,19 +15,38 @@ module Jekyll
     end
   end
 
+  # 태그 모음 추출 헬퍼
+  module TagHelper
+    def self.collect_all_tags(site)
+      tag_map = Hash.new { |h, k| h[k] = [] }
+
+      site.collections.each_value do |collection|
+        collection.docs.each do |doc|
+          tags = Array(doc.data['tags'])
+          tags.each { |t| tag_map[t] << doc }
+        end
+      end
+
+      tag_map
+    end
+  end
+
   # 모든 태그에 대해 TagPage 생성
   class TagGenerator < Generator
     safe true
     priority :low
 
     def generate(site)
-      site.tags.keys.each do |tag|
+      tags = TagHelper.collect_all_tags(site)
+      tags.keys.each do |tag|
         # 태그 이름을 슬러그화: 공백 → -, 대문자 → 소문자
         slug = tag.to_s.downcase.strip.gsub(' ', '-')
         # tags/<slug>/index.html
         dir  = File.join('tags', slug)
         site.pages << TagPage.new(site, site.source, dir, tag)
       end
+      # 사이트 전역에서도 사용 가능하도록 저장
+      site.config['generated_tags'] = tags
     end
   end
 end 
